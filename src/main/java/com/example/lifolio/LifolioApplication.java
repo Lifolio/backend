78f22e2d1f2e255a5819e2be38de19647a1a4a41
awesominki <oversteam@naver.com>
2022-12-26 15:37:20 +0900

    초기 세팅

---
+++ src/main/java/com/example/lifolio/LifolioApplication.java
@@ -0,0 +1,13 @@
+package com.example.lifolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifolioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lifolio/LifolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lifolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
