5ac533875a50f32f77bf0943db282ca30aa8c895
se-ize <seizenim@gmail.com>
2023-01-07 17:59:28 +0900

    Feat: parentCategory, subCategory 추가

--- src/main/java/com/example/lifolio/entity/Category.java
+++ src/main/java/com/example/lifolio/entity/Category.java
@@ -4,6 +4,8 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -28,6 +30,26 @@ public class Category implements Serializable {
 
  @Column(name = "title")
  private String title;
+
+ @ManyToOne (fetch = FetchType.LAZY)
+ @JoinColumn (name ="parent_cagegory_id")
+ private Category parentCategory;
+ //계층형 구조를 위해 자신의 PK를 부모로 삼는 외래키(연관관계 주인)
+
+ @OneToMany (mappedBy = "parentCategory", cascade = CascadeType.ALL)
+ private List<Category> subCategory = new ArrayList<>();
+
+ private Integer level;
+
+ @Builder
+ public Category(Long id, Long userId, Long colorId, String title, Integer level,Category parentCategory) {
+  this.id = id;
+  this.userId = userId;
+  this.colorId = colorId;
+  this.title = title;
+  this.level = level;
+  this.parentCategory = parentCategory;
+ }
 
  public void updateCategory(Long id, Long userId, Long colorId, String title){
   this.id = id;
