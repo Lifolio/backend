08125c600b30afbe4c45854dd9cc40e7935aec51
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2023-01-09 14:32:45 +0900

    Seri (#41)
    Co-authored-by: se-ize <seizenim@gmail.com>
    Co-authored-by: se-ize <70623290+se-ize@users.noreply.github.com>

---
+++ src/main/java/com/example/lifolio/dto/category/CategoryDTO.java
@@ -0,0 +1,53 @@
+package com.example.lifolio.dto.category;
+
+import com.example.lifolio.entity.Category;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CategoryDTO {
+    private Long categoryId;
+    private String branch;
+    private Long userId;
+    private Long colorId;
+    private String title;
+    private String parentCategoryName;
+    private Integer level;
+    private Map<String, CategoryDTO> children;
+
+
+    public CategoryDTO(Category entity) {
+        this.categoryId = entity.getId();
+        this.branch = entity.getBranch();
+        this.userId = entity.getUserId();
+        this.colorId = entity.getColorId();
+        this.title = entity.getTitle();
+        this.level = entity.getLevel();
+        if (entity.getParentCategory() == null) {
+            this.parentCategoryName = "대분류";
+        } else {
+            this.parentCategoryName = entity.getParentCategory().getTitle();
+        }
+
+        this.children = entity.getSubCategory() == null ? null :
+                entity.getSubCategory().stream().collect(Collectors.toMap(
+                        Category::getTitle, CategoryDTO::new
+                ));
+    }
+
+    public Category toEntity() {
+        return Category.builder()
+                .branch(branch)
+                .userId(userId)
+                .colorId(colorId)
+                .title(title)
+                .level(level)
+                .build();
+    }
+}
