2b41ad59d10886fad651567a095072a24acf2f4e
YuJin Kwon <tokyj515@naver.com>
2023-01-09 15:38:01 +0900

    Refactor : "categoryService 오타 수정"

--- src/main/java/com/example/lifolio/controller/CategoryController.java
+++ src/main/java/com/example/lifolio/controller/CategoryController.java
@@ -1,42 +1,41 @@
 package com.example.lifolio.controller;
 
 import com.example.lifolio.dto.category.CategoryDTO;
-import com.example.lifolio.service.CategorySerivce;
+import com.example.lifolio.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.constraints.NotBlank;
 import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/home")
 public class CategoryController {
-    private final CategorySerivce categorySerivce;
+    private final CategoryService categoryService;
 
     @ResponseBody
     @PostMapping("/categories")
     public Long createCategory(CategoryDTO categoryDTO) {
-        return categorySerivce.createCategory(categoryDTO);
+        return categoryService.createCategory(categoryDTO);
     }
 
     @ResponseBody
     @GetMapping("/categories/{branch}")
     public Map<String, CategoryDTO> readCategory(@PathVariable String branch) {
-        return categorySerivce.readCategory(branch);
+        return categoryService.readCategory(branch);
     }
 
     @ResponseBody
     @PatchMapping("/categories/{id}")
     public Long updateCategory (@PathVariable("categoryId") Long id,
                                 CategoryDTO categoryDTO) {
-        return categorySerivce.updateCategory(id, categoryDTO);
+        return categoryService.updateCategory(id, categoryDTO);
     }
 
     @ResponseBody
     @DeleteMapping("/categories/{id}")
     public void deleteCategory (@PathVariable("categoryId") Long id) {
-        categorySerivce.deleteCategory(id);
+        categoryService.deleteCategory(id);
     }
 
 
