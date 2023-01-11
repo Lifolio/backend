781a9260723d81f6651609c0db459092d8b6ff3a
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2023-01-11 19:15:09 +0900

    Feat : "Alarm" 관련 클래스 추가 및 엔티티 수정

---
+++ src/main/java/com/example/lifolio/controller/AlarmController.java
@@ -0,0 +1,12 @@
+package com.example.lifolio.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/alarm")
+public class AlarmController {
+}
