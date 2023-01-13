e83191e608a0c7c3625c7c8d162a9cef104dea5e
mac <oversteam@naver.com>
2023-01-13 17:14:28 +0900

    Feat : "알람 설정 구현"

--- src/main/java/com/example/lifolio/controller/AlarmController.java
+++ src/main/java/com/example/lifolio/controller/AlarmController.java
@@ -57,7 +57,122 @@ public class AlarmController {
         }
     }
 
+    @ResponseBody
+    @PatchMapping("/weekAlarm/{userId}")
+    public BaseResponse<String> updateWeekAlarm(@RequestBody AlarmReq.AllAlarmUpdateReq allAlarmUpdateReq){
+        try {
+            Long userId=tokenProvider.getUserIdx();
+            alarmService.setWeekAlarm(userId,allAlarmUpdateReq);
+            return new BaseResponse<>("수정 성공.");
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
+    }
 
+    @ResponseBody
+    @PatchMapping("/badgeAlarm/{userId}")
+    public BaseResponse<String> updateBadgeAlarm(@RequestBody AlarmReq.AllAlarmUpdateReq allAlarmUpdateReq){
+        try {
+            Long userId=tokenProvider.getUserIdx();
+            alarmService.setBadgeAlarm(userId,allAlarmUpdateReq);
+            return new BaseResponse<>("수정 성공.");
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
+    }
 
+    @ResponseBody
+    @PatchMapping("/plannigAllAlarm/{userId}")
+    public BaseResponse<String> updatePlanningAllAlarm(@RequestBody AlarmReq.AllAlarmUpdateReq allAlarmUpdateReq){
+        try {
+            Long userId=tokenProvider.getUserIdx();
+            alarmService.setPlanningAllAlarm(userId,allAlarmUpdateReq);
+            return new BaseResponse<>("수정 성공.");
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
+    }
 
+    @ResponseBody
+    @PatchMapping("/todoAlarm/{userId}")
+    public BaseResponse<String> updateTodoAlarm(@RequestBody AlarmReq.AllAlarmUpdateReq allAlarmUpdateReq){
+        try {
+            Long userId=tokenProvider.getUserIdx();
+            alarmService.setTodoAlarm(userId,allAlarmUpdateReq);
+            return new BaseResponse<>("수정 성공.");
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
+    }
+
+    @ResponseBody
+    @PatchMapping("/goalAlarm/{userId}")
+    public BaseResponse<String> updateGoalAlarm(@RequestBody AlarmReq.AllAlarmUpdateReq allAlarmUpdateReq){
+        try {
+            Long userId=tokenProvider.getUserIdx();
+            alarmService.setGoalAlarm(userId,allAlarmUpdateReq);
+            return new BaseResponse<>("수정 성공.");
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
+    }
+
+    @ResponseBody
+    @PatchMapping("/socialAllAlarm/{userId}")
+    public BaseResponse<String> updateSocialAllAlarm(@RequestBody AlarmReq.AllAlarmUpdateReq allAlarmUpdateReq){
+        try {
+            Long userId=tokenProvider.getUserIdx();
+            alarmService.setSocialAllAlarm(userId,allAlarmUpdateReq);
+            return new BaseResponse<>("수정 성공.");
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
+    }
+
+    @ResponseBody
+    @PatchMapping("/uploadAlarm/{userId}")
+    public BaseResponse<String> updateUploadAlarm(@RequestBody AlarmReq.AllAlarmUpdateReq allAlarmUpdateReq){
+        try {
+            Long userId=tokenProvider.getUserIdx();
+            alarmService.setUploadAlarm(userId,allAlarmUpdateReq);
+            return new BaseResponse<>("수정 성공.");
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
+    }
+
+    @ResponseBody
+    @PatchMapping("/interestAlarm/{userId}")
+    public BaseResponse<String> updateInterestAlarm(@RequestBody AlarmReq.AllAlarmUpdateReq allAlarmUpdateReq){
+        try {
+            Long userId=tokenProvider.getUserIdx();
+            alarmService.setInterestAlarm(userId,allAlarmUpdateReq);
+            return new BaseResponse<>("수정 성공.");
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
+    }
+
+    @ResponseBody
+    @PatchMapping("/likeAlarm/{userId}")
+    public BaseResponse<String> updateLikeAlarm(@RequestBody AlarmReq.AllAlarmUpdateReq allAlarmUpdateReq){
+        try {
+            Long userId=tokenProvider.getUserIdx();
+            alarmService.setLikeAlarm(userId,allAlarmUpdateReq);
+            return new BaseResponse<>("수정 성공.");
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
+    }
+    @ResponseBody
+    @PatchMapping("/marketingAlarm/{userId}")
+    public BaseResponse<String> updateMarketingAlarm(@RequestBody AlarmReq.AllAlarmUpdateReq allAlarmUpdateReq){
+        try {
+            Long userId=tokenProvider.getUserIdx();
+            alarmService.setMarketingAlarm(userId,allAlarmUpdateReq);
+            return new BaseResponse<>("수정 성공.");
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
+    }
 }
