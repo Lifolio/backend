2c221adb3f3c14f3229b17223d5ad47d97dd0442
임현우 <gusdn8926@naver.com>
2023-01-25 15:40:20 +0900

    Feat : PlanOfYear 엔티티 생성

---
+++ src/main/java/com/example/lifolio/entity/PlanOfYear.java
@@ -0,0 +1,36 @@
+package com.example.lifolio.entity;
+
+import com.example.lifolio.base.BaseEntity;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Getter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "PlanOfYear")
+public class PlanOfYear extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name="user_id")
+    private Long userId;
+
+    @Column(name="title")
+    private String title;
+
+    @Column(name="plan_year")
+    private String planYear;
+
+    @Column(name="plan_month")
+    private String planMonth;
+}
