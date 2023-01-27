fc154d81aec9e18bfffc0474167bb6ecb966e15e
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2023-01-27 17:38:12 +0900

    Refactor : GoalOfYear 에러 수정

--- src/main/java/com/example/lifolio/repository/GoalOfYearRepository.java
+++ src/main/java/com/example/lifolio/repository/GoalOfYearRepository.java
@@ -4,12 +4,14 @@ import com.example.lifolio.entity.GoalOfYear;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface GoalOfYearRepository extends JpaRepository<GoalOfYear, Long> {
 
-    GoalOfYear findByUserIdAndYear(Long userId, int year);
+    Optional<GoalOfYear> findByUserIdAndYear(Long userId, int year);
 
     List<GoalOfYear> findAllByUserIdOrderByYearAsc(Long userId);
 
 
+    Optional<GoalOfYear> findTop1ByUserIdAndYearOrderByCreatedAtDesc(Long userId, int year);
 }
